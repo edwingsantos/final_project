21ae1eaf5537f400d4591623d2433fce4b27a9cd
titaniumlizard445 <parker.swenson@ucas-edu.net>
2026-04-21 09:24:36 -0600

    improved folder and file structure and created a deck of cards as well as a function that replaces it if it breaks

---
+++ files/card_loader.py
@@ -0,0 +1,34 @@
+#USE IF JSON CARDS ARE WIPED
+import json
+
+
+def replace_deck_of_cards():
+    suits = ["Clubs","Spades","Diamonds","Hearts"]
+    values = ["Ace","2","3","4","5","6","7","8","9","10","Jack","Queen","King"]
+    #create all the cards
+    deck = {}
+    #for each suit
+    for x in suits:
+        #create a card for each value
+        for y in values:
+            
+            name = f"{y} of {x}"
+
+            #determines if the card is red or black
+            if suits.index(x) < 2:
+                color = "Black"
+            else:
+                color = "Red"
+            
+            #adds card to deck
+            deck[name] = {
+                "Value":y,
+                "Suit":x,
+                "Color":color}
+
+    with open("files/cards.json","w") as file:
+        file.truncate(0)
+        file.seek(0)
+        json.dump(deck,file,indent=4)
+
+replace_deck_of_cards()
