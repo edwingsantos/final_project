d138a49fd7d40a582b2b27b72ad697d91c08da78
titaniumlizard445 <parker.swenson@ucas-edu.net>
2026-04-23 09:53:15 -0600

    Did some placebo code

---
+++ src/pyramid_solitaire.py
@@ -0,0 +1,14 @@
+#PS 1st CP2 Final - Pyramid Soliaire source code
+
+#import shuffle function
+#import csv management functions
+
+#setup function
+    #call shuffle function to return a randomized list
+    #create 7 lists for each row of the pyramid and put those inside another creating a matrix
+    
+    #count each iteration of the loop to determine number of cards in each row
+    #Loop for every row (7 times)
+        #put one card in row one and then 2 cards and row to and so on
+
+    #DISPLAY TABLEU - have each row slightly offsetted
