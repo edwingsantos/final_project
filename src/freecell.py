689d04553c642e144cc189e7f8de57f17fc3cb7a
lulu094 <lucci.venanciomostaceli@ucas-edu.net>
2026-04-24 08:59:43 -0600

    wrote pseudocode

---
+++ src/freecell.py
@@ -0,0 +1,34 @@
+# LV 1st Freecell
+# import random
+# import csv
+# llamar al file de las cartas para poder hacer que funcione Shuffle
+
+# Shuffle Function
+# List of cards 1- 52 then randomize
+# Vamos a explicar mas a detalle una vez que entienda como funciona
+# El goal es que las cartas de 1-52 sean dadas al hazar 
+# Tal vez  haga una lista con las cartas y una vez dadas les damos valor 
+
+# Show game board- derepente es la parte de Parker no necesariamente
+# Once the cards are shuffled the player will be able to see the board and how the cards are being aranged
+
+# Set up game board
+# 7 filas 
+
+# Moving Function
+# Make it so the player according to the board can choose to move one card 
+# get Lizzie's function - makes sure if card is less than the card in the pile and opposite color
+# if valid just let them do the move
+# else
+# invalid move just print Error,can't move that card
+
+
+# Update game stats
+# La verdad es que no tengo ni la menor idea de lo que tiene que hacer pero esta aqui
+# cuando vulva voy a empezar a hacer la documentacion para esta parte para que tenga sentido con el codigo
+
+# Win Function
+# Asegurarnos que todas las 52 cartas sean usadas y volteadas y que las A todas esten como escalerita
+# Y que todas lleguen a King como escalerita
+# Si tiene todos esos requerimientos entonces gana
+# no importa si gana o no gana tiene que ser guardado en el CSV y poner lo mas alto primero
