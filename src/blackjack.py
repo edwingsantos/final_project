19dc62c24f1a4ea88dcc76744132a6d00f496b87
edwingsantos <edwing.santos@ucas-edu.net>
2026-04-24 23:18:02 -0600

    just finished my planing

---
+++ src/blackjack.py
@@ -0,0 +1,49 @@
+#ES starting spedocode
+
+# import pygame, json, sys 
+
+
+#call the shufle funtion from lucy and append it to the points dictionary 
+# do the same thing but just append it to the dealers hand 
+
+
+#use dictionary called point to safe the cards choosen 
+#make a dictionary for the dealers hand 
+#use an other dictionary for the amount of games played 
+#use another dictianry for win games
+#user another library for money 
+#check if the 3rd line of the csv is 0
+# if the 3rd line is empty give the user 100$ and append it to the money dictionary 
+
+
+#SET up screen 
+#set up the board with two cards you can see, these cards have to match with the ones in the points dicitonary 
+#Have two card for the dealer and make one card show, remember one of those have to match in the dictionary
+
+#make a funtion for blackjack 
+    #call the beggingin of the betting funtion 
+    #safe those amounts into the ductionaries 
+
+    #show users their cards and let them see the dealers cards
+    #if the user has an instant 21 call the winning funtion from betting funtion 
+
+
+    #make a funtion for DRAW cards
+    #ask user if they want to draw a card
+    #if they do call the funtion again and add the points to the libraries
+    #if not then continue with the code
+
+    #make a funtion for reveal cards
+    #if the dealer have cards values less than 16 call the draw funtion but add it to the dealer dictionary
+    #if the dealer has a 21 then the dealer wins, call the loosing funtion
+    #if the dealer has more than 16 then compare the two values of the dictionaries
+        #if the dealer has a grater amount of points, call the loosing funtion
+        #if the dealer has a less amount of points, call the winning funtion
+    #if the users have the same amount of points call the tie funtion from the betting funtion 
+
+
+#ask the user if they want to play blackjack again
+    #if choice is yes then call the blackjack funtion
+    #if choice is no the go to the main menu
+
+#call the blackjack funtino 
