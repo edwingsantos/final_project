de67aa9d3e19e400b3afb5e08eaa4d9eaf8a4745
edwingsantos <edwing.santos@ucas-edu.net>
2026-04-30 09:33:28 -0600

    just going to call some things

--- src/blackjack.py
+++ src/blackjack.py
@@ -4,7 +4,7 @@
 import pygame
 import json
 import sys
-#from betting_func import *
+from betting_func import *
 
 #call the shufle funtion from lucci and append it to the points dictionary 
 # do the same thing but just append it to the dealers hand 
@@ -27,8 +27,7 @@ import sys
 def blackjack_game():
     print("blackjack")
     #call the beggingin of the betting funtion 
-
-
+    starting_bet()
     #safe those amounts into the ductionaries 
 
     #show users their cards and let them see the dealers cards
@@ -53,4 +52,83 @@ def blackjack_game():
     #if choice is yes then call the blackjack funtion
     #if choice is no the go to the main menu
 
-blackjack_game()
+
+
+
+
+import tkinter as tk
+
+def show_choices(root):
+# Still have to add when to call the funcitons for each game !!!!!
+    # Title label
+    instruction = tk.Label(root, text="Choose a Game")
+    instruction.pack()
+
+    # Solitaire button
+    solitaire = tk.Button(
+        root,
+        text="Solitaire",
+        command=lambda: print("Launch Solitaire")  # replace later
+    )
+    solitaire.pack()
+
+    # Black Jack button
+    black_jack = tk.Button(
+        root,
+        text="Black Jack",
+        command=lambda: blackjack_game()
+    )
+    black_jack.pack()
+
+    # Pyramid Solitaire button
+    pyramid = tk.Button(
+        root,
+        text="Pyramid Solitaire",
+        command=lambda: print("Launch Pyramid Solitaire")
+    )
+    pyramid.pack()
+
+    # Freecell button
+    freecell = tk.Button(
+        root,
+        text="Freecell",
+        command=lambda: print("Launch Freecell")
+    )
+    freecell.pack()
+
+    # Poker button
+    poker = tk.Button(
+        root,
+        text="Poker",
+        command=lambda: print("Launch Poker")
+    )
+    poker.pack()
+
+    # Quit button
+    quit_button = tk.Button(
+        root,
+        text="Quit",
+        command=root.quit
+    )
+    quit_button.pack()
+
+def main():
+
+    root = tk.Tk()
+    root.title("Cardds Game")
+    root.geometry("300x300")
+
+    welcome = tk.Label(root, text="Welcome! Click continue to start")
+    welcome.pack()
+
+    def start_menu():
+        welcome.destroy()
+        continue_btn.destroy()
+        show_choices(root)
+
+    continue_btn = tk.Button(root, text="Continue", command=start_menu)
+    continue_btn.pack()
+
+    root.mainloop()
+    
+main()
