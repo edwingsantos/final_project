7ecf4d350be5f39f3f8f23b4e4898a6a85e62e25
lulu094 <lucci.venanciomostaceli@ucas-edu.net>
2026-05-07 09:48:25 -0600

    main calls the betting function and solitaire

--- src/solitaire.py
+++ src/solitaire.py
@@ -277,8 +277,9 @@ def game_loop():
 
         pygame.display.flip()
 
-    pygame.quit()
-game_loop()
+    #pygame.quit()
+#game_loop()
 
 # Por ahora lo que puede hacer el codigo es correr y ostrar las cartas aun no muestra las instrucciones
 # Y no me deja mover las cartas aun 
+# Write instructions for Solitaire
